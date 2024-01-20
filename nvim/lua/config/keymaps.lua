cd215e216c55bf2fdff40ae2c879183b8fb83984
xavierchanth <xchanthavong@gmail.com>
2024-01-21 01:31:14 +0800

    add neotree focus behaviour command

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Neo-tree : Open & focus, or close if already focused
+-- vim.keymap.set("n", "<leader>fe", function()
+--   local renderer = require("neo-tree.ui.renderer")
+--   local Util = require("lazyvim.util")
+--   local cmd = require("neo-tree.command")
+--   local state = require("neo-tree.sources.manager").get_state("filesystem")
+--   if renderer.window_exists(state) then
+--     if state.winid == vim.api.nvim_get_current_win() then
+--       renderer.close(state)
+--     else
+--       cmd.execute({ focus = true, dir = Util.root() })
+--     end
+--   else
+--     cmd.execute({ focus = true, dir = Util.root() })
+--   end
+-- end, { noremap = true, silent = true })
