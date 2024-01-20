4a55790faf744d4027b88eee72a8bce87833a42a
xavierchanth <xchanthavong@gmail.com>
2024-01-21 00:31:55 +0800

    remove nvim-old

--- nvim-old/init.lua
+++
@@ -1,7 +0,0 @@
-if vim.g.vscode then
-    require('xavierchanth.global');
-    require("xavierchanth.vscode")
-else
-    require('xavierchanth.global');
-    require('xavierchanth')
-end
--- nvim-old/lua/xavierchanth/vscode.lua
+++
@@ -1,6 +0,0 @@
-require('xavierchanth.lazy-init')
-
-require('lazy').setup({
-  require('xavierchanth.config.global'),
-})
-
