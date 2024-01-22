bf04e9568e10fe4d24d19865e3b59ca96e173b5d
xavierchanth <xchanthavong@gmail.com>
2024-01-22 13:58:12 +0800

    make flutter tools not lazy

--- nvim/lua/plugins/flutter.lua
+++ nvim/lua/plugins/flutter.lua
@@ -1,9 +1,8 @@
 return {
   {
     "akinsho/flutter-tools.nvim",
+    lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("flutter-tools").setup({})
-    end,
+    config = true,
   },
 }
