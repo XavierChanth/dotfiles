1769b2f1f79b21e47f3f4e4e31769ee29192643e
xavierchanth <xchanthavong@gmail.com>
2024-01-22 16:44:10 +0800

    add lazy git config

--- restore-macos.sh
+++ restore-macos.sh
@@ -4,34 +4,36 @@ FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 
 # check if brew is installed
-if ! command -v brew &> /dev/null
-then
-    echo "brew could not be found"
-    echo "install brew from https://brew.sh/"
-    exit
+if ! command -v brew &>/dev/null; then
+	echo "brew could not be found"
+	echo "install brew from https://brew.sh/"
+	exit
 fi
 
 if [ ! -f "$SCRIPT_DIRECTORY/confirmed_overwrite" ]; then
-    echo "This script could potentially overwrite your existing dotfiles."
-    echo "Are you sure you want to continue? (y/n)"
-    read -r response
-    if [[ ! "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
-        echo "Exiting..."
-        exit
-    fi
-    touch "$SCRIPT_DIRECTORY/confirmed_overwrite"
+	echo "This script could potentially overwrite your existing dotfiles."
+	echo "Are you sure you want to continue? (y/n)"
+	read -r response
+	if [[ ! "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+		echo "Exiting..."
+		exit
+	fi
+	touch "$SCRIPT_DIRECTORY/confirmed_overwrite"
 fi
 
 # zsh
-ln -sfh "$SCRIPT_DIRECTORY"/.zshrc  "$HOME"/.zshrc
+ln -sfh "$SCRIPT_DIRECTORY"/.zshrc "$HOME"/.zshrc
 touch "$SCRIPT_DIRECTORY"/zsh/secrets.sh
-ln -sFh "$SCRIPT_DIRECTORY"/zsh  "$HOME"/.config/zsh
+ln -sFh "$SCRIPT_DIRECTORY"/zsh "$HOME"/.config/zsh
 
 # antigen
-curl -fsSL git.io/antigen > "$HOME"/.config/zsh/antigen.zsh
+curl -fsSL git.io/antigen >"$HOME"/.config/zsh/antigen.zsh
 
 # install from brewfile
 brew bundle --file="$SCRIPT_DIRECTORY"/brew/Brewfile
+
+# lazygit
+ln -sFh "$SCRIPT_DIRECTORY"/lazygit "$HOME"/.config/lazygit
 
 # nvim
 ln -sFh "$SCRIPT_DIRECTORY"/nvim "$HOME"/.config/nvim
