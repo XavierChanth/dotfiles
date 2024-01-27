9f5eef1a349ce87d579d1f1ce888c6785c66a4ff
xavierchanth <xchanthavong@gmail.com>
2024-01-27 13:53:28 -0500

    support renames across packages
    doesnt work with bin folder... have to figure out why

--- nvim/lua/plugins/flutter.lua
+++ nvim/lua/plugins/flutter.lua
@@ -11,6 +11,23 @@ return {
         init_options = {
           onlyAnalyzeProjectsWithOpenFiles = false,
         },
+        settings = {
+          -- renameFilesWithClasses = "always",
+        },
+        on_attach = function()
+          -- Lazy way to add workspace folder
+          vim.lsp.buf.add_workspace_folder(vim.loop.cwd())
+          -- Alternatively, add all root patterns as workspace folders
+          -- local conf = require("flutter-tools.config")
+          -- local path = vim.loop.cwd()
+          -- local ws_roots = vim.fs.find(conf.root_patterns, { path = path, limit = 25 })
+          --   for _, v in pairs(ws_roots) do
+          --     local old_print = print
+          --     print = function() end
+          --     vim.lsp.buf.add_workspace_folder(vim.fs.dirname(v))
+          --     print = old_print
+          -- end
+        end,
       },
     },
   },
