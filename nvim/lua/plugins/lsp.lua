a1e292bd3c7482a073a70eef201315853cf61bc5
xavierchanth <xchanthavong@gmail.com>
2024-02-01 13:13:28 -0500

    update clangd settings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,6 +7,20 @@ return {
           capabilities = {
             offsetEncoding = { "utf-16" },
           },
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
         },
       },
     },
