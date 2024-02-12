4f22afebba8a790a0abee7d8ceb669f2aa924ee0
xavierchanth <xchanthavong@gmail.com>
2024-02-12 14:39:02 -0500

    chore: update config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,10 +2,33 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
+local Util = require("lazyvim.util")
 
 require("which-key").register({
   ["<leader>r"] = { name = "run" },
 })
+
+local tmux_session_command = function()
+  local session = require("persistence").get_current()
+  if session == nil then
+    return "tmux"
+  end
+  session = session:match("([^/]+)$")
+  session = session:gsub("%.", "-")
+  session = session:gsub("%%", "_")
+  print("loading tmux session: " .. session)
+  return { "tmux", "new-session", "-A", "-s", session }
+end
+
+local lazyterm = function()
+  Util.terminal(tmux_session_command(), { cwd = Util.root() })
+end
+map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)", noremap = true })
+map("n", "<leader>fT", function()
+  Util.terminal(tmux_session_command())
+end, { desc = "Terminal (cwd)", noremap = true })
+map("n", "<c-/>", lazyterm, { desc = "Terminal (root dir)", noremap = true })
+map("n", "<c-_>", lazyterm, { desc = "which_key_ignore", noremap = true })
 
 -- Shift + Space = Space in Terminal Mode
 map("t", "<S-Space>", "<Space>", { noremap = true })
