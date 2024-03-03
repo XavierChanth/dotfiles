e3760b1f062fca3c3ba93a72bb744b9b2f2f46bf
xavierchanth <xchanthavong@gmail.com>
2024-03-03 12:16:28 +0800

    fix flutter lsp

--- .config/nvim/lua/plugins/flutter.lua
+++ .config/nvim/lua/plugins/flutter.lua
@@ -17,24 +17,6 @@ return {
           lineLength = 120,
           -- renameFilesWithClasses = "always",
         },
-        on_attach = function()
-          local session = require("persistence").get_current()
-          local is_atmono, _ = session:find("at_mono")
-          -- at_mono is a BIG project, so we don't want to analyze the whole thing
-          onlyAnalyzeProjectsWithOpenFiles = (is_atmono > 0 and true or false)
-          -- Lazy way to add workspace folder
-          vim.lsp.buf.add_workspace_folder(vim.loop.cwd())
-          -- Alternatively, add all root patterns as workspace folders
-          -- local conf = require("flutter-tools.config")
-          -- local path = vim.loop.cwd()
-          -- local ws_roots = vim.fs.find(conf.root_patterns, { path = path, limit = 25 })
-          --   for _, v in pairs(ws_roots) do
-          --     local old_print = print
-          --     print = function() end
-          --     vim.lsp.buf.add_workspace_folder(vim.fs.dirname(v))
-          --     print = old_print
-          -- end
-        end,
       },
     },
   },
