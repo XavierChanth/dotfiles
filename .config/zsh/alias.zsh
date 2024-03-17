27640260b948541384cada63151c56d3170824ef
xavierchanth <xchanthavong@gmail.com>
2024-03-16 23:47:16 -0400

    clean up aliases

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -6,16 +6,22 @@
 alias sudo='sudo '
 
 alias ls='ls --color'
+alias ll='ls -lh'
+alias la='ls -lah'
+
 alias x64='arch -x86_64'
 alias s='source $HOME/.zshrc'
 alias q='exit'
 
 #cd
 alias ss='cd ~/src'
-alias sxc='cd ~/src/xc'
-alias saf='cd ~/src/af'
-alias dd='cd ~/dev'
-alias dnp='cd ~/dev/noports'
+c() {
+  selected=$(find $HOME/src $HOME/dev -mindepth 0 -maxdepth 2 -type d  | cat - <(echo $HOME/src $HOME/dev) | fzf)
+  if [ -z "$selected" ]; then
+    return
+  fi
+  cd $selected;
+}
 
 # tmux
 # TODO: replace this with some better sessionizer stuff maybe tmux-sessionx?
