d110d22a83291f048b3c7433ecf690d02eab24ea
xavierchanth <xchanthavong@gmail.com>
2024-03-20 15:33:35 -0400

    fix aggressive cmp behaviour

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -11,10 +11,19 @@ return {
   },
   {
     "hrsh7th/nvim-cmp",
-    opts = {
-      completion = {
-        completeopt = "menu,preview,menuone,noselect,noinsert",
-      },
-    },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.preselect = cmp.PreselectMode.None
+      opts.completion.completeopt = "menu,menuone,preview,noselect,noinsert"
+      opts.mapping = {
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+      }
+    end,
   },
 }
