04919a0a1c85c1d2f75ccac7e6093bc6d6734389
xavierchanth <xchanthavong@gmail.com>
2024-03-23 17:27:49 -0400

    optimize zsh startup time
    conda & nvm init are slow, so disable them since I don't use them often

--- .config/zsh/prog.zsh
+++ .config/zsh/prog.zsh
@@ -1,35 +1,89 @@
 #!/bin/zsh
 
+__flutter=true
+__android=true
+__node=false
+__bun=true
+__rust=false
+__clang=true
+__java=true
+__conda=false
+__zsh_hl=true
+
+# To avoid expanding $__path lots of times
+__path=""
+
 # flutter
-export PUB_CACHE="$HOME/.pub-cache"
-export FLUTTER_ROOT="$HOME/dev/flutter"
-export PATH="$PUB_CACHE/bin:$FLUTTER_ROOT/bin:$PATH"
-alias pub='dart pub'
-alias melos='dart run melos'
+if $__flutter; then
+  export PUB_CACHE="$HOME/.pub-cache"
+  export FLUTTER_ROOT="$HOME/dev/flutter"
+  __path="$PUB_CACHE/bin:$FLUTTER_ROOT/bin:$__path"
+  # dart completions
+  [[ -f $HOME/.dart-cli-completion/zsh-config.zsh ]] && . $HOME/.dart-cli-completion/zsh-config.zsh || true
+  alias pub='dart pub'
+  alias melos='dart run melos'
+fi
 
 # android
-export ANDROID_HOME="/Users/chant/Library/Android/sdk"
-export PATH="$ANDROID_HOME/cmdline-tools/latest/bin:$PATH"
+if $__android; then
+  export ANDROID_HOME="/Users/chant/Library/Android/sdk"
+  __path="$ANDROID_HOME/cmdline-tools/latest/bin:$__path"
+fi
 
 # node
- export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+if $__node; then
+  export NVM_DIR="$HOME/.nvm"
+    [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+    [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+fi
 
 # bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
+if $__bun; then
+  export BUN_INSTALL="$HOME/.bun"
+  __path="$BUN_INSTALL/bin:$__path"
+# bun completions
+[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+fi
 
 # rust
-export PATH="$HOME/.cargo/bin:$PATH"
+if $__rust; then
+  __path="$HOME/.cargo/bin:$__path"
+fi
 
 # clang
-export CPATH="/usr/local/include:/opt/homebrew/include:/opt/homebrew/opt/llvm/include:$CPATH"
-alias cmbs='cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON'
-alias cmbb='cmake --build build'
-alias cmbt='cmake --build build --target'
-alias ctb='ctest --test-dir build --output-on-failure'
+if $__clang; then
+  export CPATH="/usr/local/include:/opt/homebrew/include:/opt/homebrew/opt/llvm/include:$CPATH"
+  alias cmbs='cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON'
+  alias cmbb='cmake --build build'
+  alias cmbt='cmake --build build --target'
+  alias ctb='ctest --test-dir build --output-on-failure'
+fi
 
 # java
-export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+if $__java; then
+  export JAVA_HOME="/opt/homebrew/opt/openjdk"
+  __path="/opt/homebrew/opt/openjdk/bin:$__path"
+fi
+
+if $__conda; then
+  __conda_setup="$('/opt/homebrew/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+  if [ $? -eq 0 ]; then
+      eval "$__conda_setup"
+  else
+      if [ -f "/opt/homebrew/anaconda3/etc/profile.d/conda.sh" ]; then
+          . "/opt/homebrew/anaconda3/etc/profile.d/conda.sh"
+      else
+          export __path="/opt/homebrew/anaconda3/bin:$__path"
+      fi
+  fi
+  unset __conda_setup
+fi
+
+# syntax highlighting must be done last for it to work correctly
+if $__zsh_hl && command -v brew &>/dev/null; then
+  [[ -f $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]] && source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
+
+# append local path to PATH
+export __path="$__path:$PATH"
 
--- .zshrc
+++ .zshrc
@@ -19,31 +19,3 @@ source $XDG_CONFIG_HOME/zsh/git.zsh
 source $XDG_CONFIG_HOME/zsh/iterm2.zsh
 source $XDG_CONFIG_HOME/zsh/prog.zsh
 
-# Below here is the stuff automatically added by install scripts
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/opt/homebrew/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/homebrew/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/opt/homebrew/anaconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/homebrew/anaconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
-# dart completions
-[[ -f /Users/chant/.dart-cli-completion/zsh-config.zsh ]] && . /Users/chant/.dart-cli-completion/zsh-config.zsh || true
-
-# bun completions
-[ -s "/Users/chant/.bun/_bun" ] && source "/Users/chant/.bun/_bun"
-
-
-# syntax highlighting must be done last for it to work correctly
-if command -v brew &>/dev/null; then
-  [[ -f $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]] && source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-fi
