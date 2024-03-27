5d9034f32994c3d52936dd8002dcce382b0bba65
xavierchanth <xchanthavong@gmail.com>
2024-03-26 21:50:46 -0400

    backup

--- backup-linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
-SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
-
-rm "$SCRIPT_DIRECTORY"/.xavierchanth/linux/Brewfile
-brew bundle dump --file="$SCRIPT_DIRECTORY"/.xavierchanth/linux/Brewfile
--- restore.sh
+++ restore.sh
@@ -3,6 +3,10 @@
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 
+is_darwin() {
+	[ "$(uname)" = 'Darwin' ]
+}
+
 while getopts "hb" opt; do
 	case $opt in
 	h) echo "Usage: $0 [-b]" && exit ;;
@@ -19,7 +23,7 @@ if ! command -v brew &>/dev/null; then
 	exit
 fi
 if [ "$brew" == 1 ]; then
-	brew bundle --file="$SCRIPT_DIRECTORY"/.xavierchanth/linux/Brewfile
+	brew bundle --file="$SCRIPT_DIRECTORY"/.xavierchanth/brew/Brewfile
 fi
 
 # zsh - setup non-committed files
@@ -27,8 +31,16 @@ touch "$SCRIPT_DIRECTORY"/.config/zsh/secrets.zsh
 
 # stow
 if ! command -v stow &>/dev/null; then
-	echo "brew could not be found"
-	echo "install brew from https://brew.sh/"
+	echo "stow could not be found"
 	exit
 fi
 stow -d "$SCRIPT_DIRECTORY" -t "$HOME" .
+
+if is_darwin; then
+	# iterm2 - must use their weird way of loading config files
+	if ! [ -f "$HOME"/.iterm2_shell_integration.zsh ]; then
+		curl -L https://iterm2.com/shell_integration/zsh -o "$HOME"/.iterm2_shell_integration.zsh
+	fi
+	defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.xavierchanth/iterm2"
+	defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+fi
