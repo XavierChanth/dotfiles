9073d8b36015b5b2e812e203e3b5064cb30ea99c
xavierchanth <xchanthavong@gmail.com>
2024-04-06 10:09:21 -0400

    save brew stuff before I delete everything

--- backup.sh
+++ backup.sh
@@ -6,3 +6,4 @@ SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 _brew_file="$SCRIPT_DIRECTORY"/.xavierchanth/brew/Brewfile
 rm "$_brew_file"
 brew bundle dump --file="$_brew_file"
+brew leaves --installed-on-request >"$SCRIPT_DIRECTORY"/.xavierchanth/brew/leaves.txt
