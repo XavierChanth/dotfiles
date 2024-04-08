69f5c68e298fe891c314ee47a7fa1d62f17ab6e3
xavierchanth <xchanthavong@gmail.com>
2024-04-08 17:51:07 -0400

    use hostname when setting up apollo container

--- .xavierchanth/docker/run-devenv.sh
+++ .xavierchanth/docker/run-devenv.sh
@@ -3,7 +3,7 @@
 if $(docker ps --all | grep xavierchanth:devenv | grep -q apollo); then
   docker start -ai apollo
 else
-  docker run -it --name apollo \
+  docker run -it --name apollo --hostname apollo \
     --mount type=bind,source="$HOME"/.ssh,target=/home/chant/.ssh \
     --mount type=bind,source="$HOME"/.atsign,target=/home/chant/.atsign \
     --mount type=bind,source="$HOME"/src,target=/home/chant/src \
