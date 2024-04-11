05ff095d01f7d035c4d02f3ff8d29efa13865b5f
xavierchanth <xchanthavong@gmail.com>
2024-04-11 10:27:38 -0400

    run devenv detached

--- .local/bin/run-devenv.sh
+++ .local/bin/run-devenv.sh
@@ -11,7 +11,7 @@ if $(docker ps --all | grep xavierchanth/devenv | grep -q "$name"); then
   docker start -ai "$name"
 else
   echo "creating new $name container"
-  docker run -it --name "$name" --hostname "$name" \
+  docker run -d --name "$name" --hostname "$name" \
     --mount type=bind,source="$HOME"/.ssh,target=/home/chant/.ssh \
     --mount type=bind,source="$HOME"/.atsign,target=/home/chant/.atsign \
     --mount type=bind,source="$HOME"/src,target=/home/chant/src \
