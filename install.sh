6919c0e7a51d06818773dbd1d956efd8bdf6c09b
xavierchanth <xchanthavong@gmail.com>
2024-04-16 19:19:13 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+script_dir="$(dirname -- "$(readlink -f -- "$0")")"
 
 # Make files and directories used but not "owned" by dotfiles
 touch "$HOME"/.zshenv
@@ -9,15 +10,10 @@ mkdir -p "$HOME/.local/bin"
 mkdir -p "$HOME/.local/asdf"
 
 # ensure git is installed
-if command -v "git" >/dev/null; then
+if ! command -v git &>/dev/null; then
   echo "git not found, install git before continuing..."
   echo "how did you even git this onto your machine?"
   exit 1
-fi
-
-if ! command -v stow &>/dev/null; then
-  echo "stow not found, install gnu stow before continuing..."
-  exit 2
 fi
 
 # git global settings
@@ -39,24 +35,36 @@ git config --global pull.ff only
 
 # install brew
 if [ "$(uname)" = 'Darwin' ]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if ! command -v brew &>/dev/null; then
+    echo "brew not found, installing it for you..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
   /opt/homebrew/bin/brew install stow
-  stow -d "$SCRIPT_DIRECTORY/Library" -t "$HOME/Library" .
+  stow -d "$script_dir/Library" -t "$HOME/Library" .
   defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.xavierchanth/iterm2"
   defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+fi
+
+if ! command -v stow &>/dev/null; then
+  echo "stow not found, install gnu stow before continuing..."
+  exit 2
 fi
 
 # install asdf
 asdf_path="$HOME/.local/asdf"
 git clone https://github.com/asdf-vm/asdf.git "$asdf_path" --branch v0.14.0
+. "$asdf_path/asdf.sh"
 
 asdf_install() {
   if [ -z "$2" ]; then
-    2='latest'
+    _version='latest'
+  else
+    _version="$2"
   fi
   "$asdf_path/bin/asdf" plugin add "$1"
-  "$asdf_path/bin/asdf" install "$1" "$2"
-  "$asdf_path/bin/asdf" local "$1" "$2"
+  "$asdf_path/bin/asdf" install "$1" "$_version"
+  "$asdf_path/bin/asdf" local "$1" "$_version"
 }
 
 asdf_install fzf
@@ -70,7 +78,7 @@ asdf_install neovim stable
 git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.config/spaceship-prompt"
 
 # sync dotfiles
-stow -d "$SCRIPT_DIRECTORY" -t "$HOME" .
+stow -d "$script_dir" -t "$HOME" .
 
 # additional programming languages worth installing
 asdf_install flutter
@@ -79,8 +87,9 @@ asdf_install jq
 asdf_install python
 asdf_install uv
 asdf_install cmake
+asdf_install nodejs
 
 # sync nvim
-RUN nvim --headless "+Lazy! sync" +qa
+nvim --headless "+Lazy! sync" +qa
 
-"$SCRIPT_DIRECTORY"/check-env.sh
+"$script_dir"/check-env.sh
