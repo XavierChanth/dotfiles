125d7fe803f9937069c3648858904ac04efe2cac
xavierchanth <xchanthavong@gmail.com>
2024-04-20 14:57:37 -0400

    rename to just devenv

--- .local/devenv/build.sh
+++ .local/devenv/build.sh
@@ -5,8 +5,4 @@ docker_build() {
   docker build --tag $1 -f "$script_dir"/Dockerfile "$script_dir"
 }
 
-docker_build devenv-arm64:latest
-(
-  DOCKER_DEFAULT_PLATFORM=linux/amd64
-  docker_build devenv-x64:latest
-)
+docker_build devenv:latest
