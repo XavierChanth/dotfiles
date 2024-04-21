964c4ec71181807586670e0ffba9a089d72bf292
xavierchanth <xchanthavong@gmail.com>
2024-04-21 16:50:29 -0400

    fix telescope

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -13,34 +13,34 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   {
     "debugloop/telescope-undo.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
     keys = {
-      { -- lazy style key map
+      {
         "<leader>uh",
         "<cmd>Telescope undo<cr>",
         desc = "undo history",
       },
     },
-    opts = {
-      extensions = {
-        undo = {
-          use_delta = true,
-          mappings = {
-            i = {
-              ["<cr>"] = require("telescope-undo.actions").restore,
-              ["<C-y>"] = require("telescope-undo.actions").yank_additions,
-              ["<C-Y>"] = require("telescope-undo.actions").yank_deletions,
-            },
-            n = {
-              ["<cr>"] = require("telescope-undo.actions").restore,
-              ["y"] = require("telescope-undo.actions").yank_additions,
-              ["Y"] = require("telescope-undo.actions").yank_deletions,
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          undo = {
+            use_delta = true,
+            mappings = {
+              i = {
+                ["<cr>"] = require("telescope-undo.actions").restore,
+                ["<C-y>"] = require("telescope-undo.actions").yank_additions,
+                ["<C-Y>"] = require("telescope-undo.actions").yank_deletions,
+              },
+              n = {
+                ["<cr>"] = require("telescope-undo.actions").restore,
+                ["y"] = require("telescope-undo.actions").yank_additions,
+                ["Y"] = require("telescope-undo.actions").yank_deletions,
+              },
             },
           },
         },
-      },
-    },
-    config = function(_, opts)
-      require("telescope").setup(opts)
+      })
       require("telescope").load_extension("undo")
     end,
   },
