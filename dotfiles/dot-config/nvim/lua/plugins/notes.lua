0c2ae96e64d889a863053876a729c195ebb01b19
xavierchanth <xchanthavong@gmail.com>
2024-05-12 13:06:55 -0400

    fix notes frontmatter function

--- dotfiles/dot-config/nvim/lua/plugins/notes.lua
+++ dotfiles/dot-config/nvim/lua/plugins/notes.lua
@@ -21,9 +21,6 @@ return {
           path = "~/src/xc/notes",
         },
       },
-      -- TODO: configure this later
-      -- https://github.com/epwalsh/obsidian.nvim
-      -- see below for full list of options 👇
       notes_subdir = "01_notes",
       attachments = {
         img_folder = "03_files",
@@ -35,7 +32,26 @@ return {
         return title
       end,
       note_frontmatter_func = function(note)
-        note.tags[#note.tags + 1] = "#atom"
+        note.metadata = note.metadata or {}
+        if note.metadata.atom == nil then
+          note.metadata.atom = true
+        end
+
+        local atom_index = 0
+        for i, tag in pairs(note.tags) do
+          if tag == "#atom" then
+            atom_index = i
+          end
+        end
+
+        if atom_index == 0 and note.metadata.atom then
+          note.tags[#note.tags + 1] = "#atom"
+        end
+
+        if atom_index ~= 0 and not note.metadata.atom then
+          note.tags[atom_index] = nil
+        end
+
         local out = { id = note.id, aliases = note.aliases, tags = note.tags }
         if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
           for k, v in pairs(note.metadata) do
