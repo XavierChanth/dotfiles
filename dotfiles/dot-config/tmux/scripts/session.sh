ee7bef9ef5a89eb737934c5ceecc70dd95c16df2
xavierchanth <xchanthavong@gmail.com>
2024-05-13 13:09:46 -0400

    tmux scripts in one place

---
+++ dotfiles/dot-config/tmux/scripts/session.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+switch_session() {
+  tmux ls -F '#S' |
+    fzf --header switch-session --preview 'tmux capture-pane -pt {}' |
+    xargs -I % tmux switch-client -t '%'
+}
+
+add_session() {
+  selected=$(
+    (
+      find "$HOME/src" "$HOME/dev" -mindepth 0 -maxdepth 2 -type d
+      echo "$HOME/.dotfiles"
+    ) |
+      fzf --scheme=path --tiebreak=end,index --header add-session
+  )
+  [ -z $selected ] && return
+  name=$(basename $selected)
+
+  if [ -z $TMUX ]; then
+    tmux new-session -Ac $selected -s $name
+    return
+  fi
+
+  tmux new-ses -APdc $selected -s $name | xargs tmux switch-client -t
+}
+
+switch_or_add_session() {
+  selected=$(
+    (
+      find "$HOME/src" "$HOME/dev" -mindepth 0 -maxdepth 2 -type d
+      echo "$HOME/.dotfiles"
+    ) |
+      fzf --scheme=path --tiebreak=end,index --header add-session
+  )
+  [ -z $selected ] && return
+  name=$(basename $selected)
+
+  echo $selected
+
+  if [ -z $TMUX ]; then
+    tmux new-session -Ac $selected -s $name
+    return
+  fi
+
+  tmux switch-client -t $name ||
+    tmux new-ses -APdc $selected -s $name | xargs tmux switch-client -t
+}
+
+delete_session() {
+  tmux ls -F '#S' |
+    fzf --header delete-session --preview 'tmux capture-pane -pt {}' |
+    xargs tmux kill-session -t
+}
--- dotfiles/dot-config/zsh/alias.sh
+++ dotfiles/dot-config/zsh/alias.sh
@@ -13,19 +13,11 @@ alias x64='arch -x86_64'
 alias s='source $HOME/.zshenv && source $HOME/.zshrc'
 alias q='exit'
 
-alias find_projects="{find $HOME/src $HOME/dev -mindepth 0 -maxdepth 2 -type d; echo "$HOME/.dotfiles"}"
-alias fzf_projects='find_projects | fzf --scheme=path --tiebreak=end,index'
-alias fzf_project='fzf_projects' # idk why but cc is truncating the last char in cc alias so...
-alias cc='__cc_selected=$(fzf_projects) || return 1 && cd $__cc_selected'
-
 alias v='nvim'
 alias vv='cc; nvim'
 
 alias t='tmux'
-alias tt='__tt_selected=$(fzf_projects) || return 1 &&
-  [ -z $TMUX ] && tmux new-ses -Ac $__tt_selected -s $(basename $__tt_selected) ||
-  tmux switch-client -t $(basename $__tt_selected) ||
-  tmux new-ses -AdPc $__tt_selected -s $(basename $__tt_selected) | xargs tmux switch-client -t'
+alias tt='source $XDG_CONFIG_HOME/tmux/scripts/session.sh && switch_or_add_session'
 
 alias td='__tt_selected=$(docker ps --all --format "table {{.Names}}" | fzf) || return 1 &&
   docker start $__tt_selected &&
