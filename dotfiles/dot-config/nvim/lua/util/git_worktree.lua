a6f55d0605ce4e40eca9a831c7ceb456f962a0be
xavierchanth <xchanthavong@gmail.com>
2024-05-12 21:00:54 -0400

    add is_inside_worktree git function

--- dotfiles/dot-config/nvim/lua/util/git_worktree.lua
+++ dotfiles/dot-config/nvim/lua/util/git_worktree.lua
@@ -1,35 +1,48 @@
 local M = {}
 
 function M.add(opts)
-	local actions = require("telescope.actions")
-	local action_state = require("telescope.actions.state")
-	opts = opts or {}
-	opts.attach_mappings = function()
-		actions.select_default:replace(function(prompt_bufnr, _)
-			local selected_entry = action_state.get_selected_entry()
-			local current_line = action_state.get_current_line()
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+  opts = opts or {}
+  opts.attach_mappings = function()
+    actions.select_default:replace(function(prompt_bufnr, _)
+      local selected_entry = action_state.get_selected_entry()
+      local current_line = action_state.get_current_line()
 
-			actions.close(prompt_bufnr)
+      actions.close(prompt_bufnr)
 
-			local branch = selected_entry ~= nil and selected_entry.value or current_line
+      local branch = selected_entry ~= nil and selected_entry.value or current_line
 
-			if branch == nil then
-				return
-			end
+      if branch == nil then
+        return
+      end
 
-			local name = branch:gsub("^origin/", "", 1)
-			local path = require("util.root").git({ bare = true }) .. "/" .. name
+      local name = branch:gsub("^origin/", "", 1)
+      local path = require("util.root").git({ bare = true }) .. "/" .. name
 
-			require("git-worktree").create_worktree(path, name)
-		end)
+      require("git-worktree").create_worktree(path, name)
+    end)
 
-		return true
-	end
-	require("telescope.builtin").git_branches(opts)
+    return true
+  end
+  require("telescope.builtin").git_branches(opts)
+end
+
+function M.is_inside_worktree(path)
+  local Job = require("plenary.job")
+
+  local res, exit_code = Job:new({
+    command = "git",
+    args = { "rev-parse", "--is-inside-work-tree" },
+    cwd = path,
+    enabled_recording = true,
+  }):sync()
+
+  return exit_code == 0 and res == "true"
 end
 
 function M.telescope()
-	require("telescope").extensions.git_worktree.git_worktrees()
+  require("telescope").extensions.git_worktree.git_worktrees()
 end
 
 return M
