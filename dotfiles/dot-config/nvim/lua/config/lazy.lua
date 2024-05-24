9d9ba3d08c63ff2fc32dc48027ad7ce3ca3f3367
xavierchanth <xchanthavong@gmail.com>
2024-05-24 10:00:02 -0400

    add yaml ls

--- dotfiles/dot-config/nvim/lua/config/lazy.lua
+++ dotfiles/dot-config/nvim/lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
   },
   defaults = {
     lazy = true,
