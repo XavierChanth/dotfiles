97adfc3430d41870941e2bf6b21875e3819516eb
xavierchanth <xchanthavong@gmail.com>
2024-06-04 22:42:04 +0000

    add removed plugin

--- dotfiles/dot-config/nvim/lua/config/lazy.lua
+++ dotfiles/dot-config/nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     },
     { import = "plugins" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.outline" },
