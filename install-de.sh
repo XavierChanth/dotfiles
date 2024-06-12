4d68ff0b46e79bf2dba2d929b37a601503d6adbc
xavierchanth <xchanthavong@gmail.com>
2024-06-11 22:58:55 -0400

    add install de script

---
+++ install-de.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+case "$(uname)" in
+  Darwin)
+    if command_exists dnf; then
+      echo "brew package manager found, installing packages for brew"
+
+      # core apps
+      brew install <<EOF
+        librewolf google-chrome
+        zoom discord spotify
+        betterdisplay cleanshot unnaturalscrollwheels
+      EOF
+      # desktop essentials
+      brew tap felixkratz/formulae
+      brew install <<EOF
+        aerospace sketchybar raycast
+      EOF
+      # terminal stuff
+      brew install <<EOF
+        wezterm asciinema
+        font-jetbrains-mono-nerd-font
+        aerc abook
+      EOF
+    fi
+    ;;
+  Linux)
+    if command_exists dnf; then
+      echo "dnf package manager found, installing packages for dnf"
+
+      sudo dnf install wezterm
+      sudo dnf install aerc abook
+    fi
+
+    ;;
+esac
