1a6eb85838d26c672f8e934bed7bc47d4acd728b
xavierchanth <xchanthavong@gmail.com>
2024-08-08 15:27:04 -0400

    better tmux delete session dialog

--- dotfiles/dot-config/tmux/scripts/session.sh
+++ dotfiles/dot-config/tmux/scripts/session.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+get_current_session() {
+  tmux list-panes -t "$TMUX_PANE" -F '#S' | head -n1
+}
+
 switch_session() {
+  current=$(get_current_session)
   tmux ls -F '#S' |
+    grep -v "^$current\$" |
     fzf --header "Switch tmux session" --preview 'tmux capture-pane -pt {}' |
     xargs -I % tmux switch-client -t '%'
 }
@@ -25,85 +31,19 @@ add_session() {
   tmux new-ses -APdc $selected -s $name | xargs tmux switch-client -t
 }
 
-switch_or_add_session() {
-  selected=$(
-    (
-      find "$HOME/src" "$HOME/dev" -mindepth 0 -maxdepth 2 -type d
-      echo "$HOME/.dotfiles"
-    ) |
-      fzf --scheme=path --tiebreak=end,index --header "Open tmux session"
-  )
-  [ -z $selected ] && return
-  name=$(basename $selected)
-
-  echo $selected
-
-  if [ -z $TMUX ]; then
-    tmux new-session -Ac $selected -s $name
-    return
-  fi
-
-  tmux switch-client -t $name ||
-    tmux new-ses -APdc $selected -s $name | xargs tmux switch-client -t
-}
-
 delete_session() {
-  tmux ls -F '#S' |
-    fzf --header "!!! DELETE tmux session !!!" --preview 'tmux capture-pane -pt {}' |
-    xargs tmux kill-session -t
-}
-
-docker_session() {
-  selected=$(docker ps --all --format "table {{.Names}}" | fzf)
-  [-z $selected ] && return
-
-  name=$(basename $selected)
-  docker start $selected
-
-  if [ -z $TMUX ]; then
-    tmux new-ses -Ac $selected -s $name
-    return
-  fi
-
-  tmux switch-client -t $name ||
-    tmux new-ses -AdPc $selected -s $name docker exec -it $name /bin/zsh | xargs tmux switch-client -t
-}
-
-ssh_session() {
-
-  hostname=$(
-    (
-      echo ""
-      grep -E "^Host ([^*]+)$" $HOME/.ssh/config | sed 's/Host //'
-    ) | fzf --header "Host"
+  selected=$(
+    tmux ls -F '#S' |
+      fzf --header "!!! DELETE tmux session !!!" --preview 'tmux capture-pane -pt {}'
   )
+  current=$(get_current_session)
 
-  if [ -z "$hostname" ]; then
-    echo "Enter custom hostname:"
-    read hostname
+  [ -z $selected ] && return
+  [ -z $current ] && return
+
+  if [ "$selected" = "$current" ]; then
+    switch_session
   fi
 
-  jumpbox=$(
-    (
-      echo ""
-      grep -E "^Host ([^*]+)$" $HOME/.ssh/config | sed 's/Host //'
-    ) | fzf --header "Jumpbox"
-  )
-
-  echo "Additional args:"
-  read args
-
-  if [ -n "$jumpbox" ]; then
-    command="ssh $USER@$hostname -J $jumpbox"
-  else
-    command="ssh $USER@$hostname"
-  fi
-
-  if [ -z $TMUX ]; then
-    tmux new-ses -As $hostname -e "TMUX_SSH_COMMAND='$command'" "$command"
-    return
-  fi
-
-  # FIXME:
-  tmux new-ses -e "TMUX_SSH_COMMAND=$command" -AdPs "$hostname" "$command" | xargs tmux switch-client -t
+  tmux kill-session -t $selected
 }
