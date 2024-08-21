566558182a4af4162a32d2038b51b0cb54527a2b
xavierchanth <xchanthavong@gmail.com>
2024-08-20 23:59:24 -0400

    fix: terminals from duping themselves in telescope

--- dotfiles/dot-config/nvim/lua/util/terminal.lua
+++ dotfiles/dot-config/nvim/lua/util/terminal.lua
@@ -4,6 +4,11 @@ M.terminals = {}
 function M.open_oil_terminal()
   local cwd = require("oil").get_current_dir()
   LazyVim.terminal(nil, { cwd = cwd })
+  for _, dir in ipairs(M.terminals) do
+    if dir == cwd then
+      return
+    end
+  end
   if cwd ~= nil then
     M.terminals[#M.terminals + 1] = cwd
   end
