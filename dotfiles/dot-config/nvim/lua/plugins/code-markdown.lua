26b1cff337eaf71757e94bcb4c0204d83ef16263
xavierchanth <xchanthavong@gmail.com>
2024-09-04 09:24:50 -0400

    add markdown extra

--- dotfiles/dot-config/nvim/lua/plugins/code-markdown.lua
+++ dotfiles/dot-config/nvim/lua/plugins/code-markdown.lua
@@ -1,13 +1,4 @@
-local path = vim.env.HOME .. "/src/xc/notes"
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
   {
     "edluffy/hologram.nvim",
     opts = {
