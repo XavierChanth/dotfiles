9c24c3786350969206316161dcd73cd717700832
xavierchanth <xchanthavong@gmail.com>
2024-09-15 00:49:56 -0400

    tmux ssh support

--- dotfiles/dot-config/tmux/scripts/session.sh
+++ dotfiles/dot-config/tmux/scripts/session.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function add_session() {
+function fzf_session() {
   selected=$(
     (
       find "$HOME/src" "$HOME/dev" -mindepth 0 -maxdepth 2 -type d
@@ -8,13 +8,39 @@ function add_session() {
     ) |
       fzf --scheme=path --tiebreak=end,index --header "Open tmux session"
   )
+
   [ -z $selected ] && return
   name=$(basename $selected)
+  tmux if-shell -F '#{==:#{pane_mode},tree-mode}' 'send q'
+  session=$(tmux new-ses -APdc $selected -s $name)
 
   if [ -z $TMUX ]; then
-    tmux new-session -Ac $selected -s $name
-    return
+    tmux attach -t $session
+  else
+    tmux switch-client -t $session
+  fi
+}
+
+function ssh_session() {
+  selected=$(
+    (
+      cat $HOME/.ssh/config | grep 'Host ' | grep -v 'Host \*' | cut -d ' ' -f2
+    ) |
+      fzf --scheme=path --tiebreak=end,index --header "Open tmux session"
+  )
+
+  [ -z $selected ] && return
+  name="ssh-$selected"
+  command="ssh $selected"
+
+  tmux if-shell -F '#{==:#{pane_mode},tree-mode}' 'send q'
+  session=$(tmux new-ses -APdc $selected -s $name)
+  tmux send-keys -t $session $command C-l C-m
+
+  if [ -z $TMUX ]; then
+    tmux attach -t $session
+  else
+    tmux switch-client -t $session
   fi
 
-  tmux new-ses -APdc $selected -s $name | xargs tmux switch-client -t
 }
