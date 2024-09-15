453aa2ec3df58e446d46176bee2e56ee2719cde0
xavierchanth <xchanthavong@gmail.com>
2024-09-15 00:49:42 -0400

    vi mode yank

--- dotfiles/dot-config/zsh/vi-mode.sh
+++ dotfiles/dot-config/zsh/vi-mode.sh
@@ -9,28 +9,48 @@ export KEYTIMEOUT=1 # decrease the delay
 _block='\e[1 q'
 _beam='\e[5 q'
 
-# vi-mode init (start in insert mode with beam cursor)
-zle-line-init() {
-  zle -K viins
-  echo -ne $_beam
-}
-zle -N zle-line-init # register init
-
-zle-keymap-select() { # change cursor when swapping keymaps
+# Override default keymap-select
+function zle-keymap-select() { # change cursor when swapping keymaps
   case $KEYMAP in
     vicmd) echo -ne $_block ;;
     viins | main) echo -ne $_beam ;;
   esac
 }
-zle -N zle-keymap-select # register keymap select
 
-# Some common zle widgets which I accidentally type
-w() {
+# Override default line-init
+function zle-line-init() {
+  zle -K viins # Start in insert keymap
+  # Start with beam cursor
+  echo -ne $_beam
+}
+
+# Yank to system
+# Don't override, because we still want all the nice default behaviors
+function vi-yank {
+  zle .vi-yank
+  if [ "$(uname)" = 'Darwin' ]; then
+    printf "$CUTBUFFER" | pbcopy
+  else
+    printf "$CUTBUFFER" | xclip -i
+  fi
+  zle -K viins
+}
+
+# Because vim things happen
+function w() {
   zle accept-line
 }
+function wq() {
+  zle accept-line
+}
+function q() {
+  zle kill-buffer
+}
+
+# Register all zle functions declared
+zle -N zle-line-init
+zle -N zle-keymap-select
+zle -N vi-yank
 zle -N w
-
-wq() {
-  zle accept-line
-}
 zle -N wq
+zle -N q
