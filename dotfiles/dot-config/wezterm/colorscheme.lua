719bc409c656e7fea1b6ad100ac223c36e7c9a5f
xavierchanth <xchanthavong@gmail.com>
2024-09-20 19:30:18 -0400

    reload wezterm colors

--- dotfiles/dot-config/wezterm/colorscheme.lua
+++ dotfiles/dot-config/wezterm/colorscheme.lua
@@ -1,3 +1,4 @@
+local wezterm = require("wezterm")
 -- Map of nvim theme names to wezterm theme names
 local themes = {
 	["catppuccin-mocha"] = "Catppuccin Mocha",
@@ -16,17 +17,31 @@ local selected = "catppuccin-mocha"
 -- Load color file
 local filename = os.getenv("HOME") .. "/.local/share/nvim/last-color"
 local f = assert(io.open(filename, "r"))
-local lastcolor = f:read("l")
-f:close()
-for index, _ in pairs(themes) do
-	if index == lastcolor then
-		selected = lastcolor
+local function read_lastcolor()
+	local lastcolor = f:read("l")
+	for index, _ in pairs(themes) do
+		if index == lastcolor then
+			selected = lastcolor
+		end
 	end
+	return lastcolor
 end
 
--- Set wezterm to auto load the last-color file
-local wezterm = require("wezterm")
-wezterm.add_to_config_reload_watch_list(filename)
+local lastcolor = read_lastcolor()
+
+local function watch_lastcolor()
+	wezterm.time.call_after(1, function()
+		local current = read_lastcolor()
+		if current ~= lastcolor then
+			f:close()
+			wezterm.reload_configuration()
+			lastcolor = current
+		end
+		watch_lastcolor()
+	end)
+end
+
+watch_lastcolor()
 
 -- Return the theme info
 return {
