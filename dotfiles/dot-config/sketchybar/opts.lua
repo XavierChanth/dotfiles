5a4fceb2a8dadcb593ceddcce591489ef08b6cb6
xavierchanth <xchanthavong@gmail.com>
2024-09-20 00:54:05 -0400

    diagonal theme

--- dotfiles/dot-config/sketchybar/opts.lua
+++ dotfiles/dot-config/sketchybar/opts.lua
@@ -1,4 +1,6 @@
 local colorscheme = require("colorscheme")
+local style = "diagonal" -- "bubbles" | "diagonal"
+
 M = {}
 
 M.merge = function(left, right)
@@ -27,35 +29,64 @@ M.font = {
 		style = "Regular",
 		size = 36.1,
 	},
+	separator2 = {
+		family = M.font_face,
+		style = "Regular",
+		size = 24.5,
+	},
 }
 
 M.get_left_separator = function(color, override)
 	override = override or {}
-	return M.merge({
-		icon = {
-			string = "",
-			color = M.color[color],
-			font = M.font.separator,
-			padding_right = -16,
-		},
-		label = { drawing = false },
-		background = { drawing = false },
-	}, override)
+	if style == "bubbles" then
+		return M.merge({
+			icon = {
+				string = "",
+				color = M.color[color],
+				font = M.font.separator,
+				padding_right = -16,
+			},
+			label = { drawing = false },
+			background = { drawing = false },
+		}, override)
+	else
+		return M.merge({
+			icon = {
+				string = "█",
+				color = M.color[color],
+				font = M.font.separator2,
+				padding_right = -2,
+			},
+			label = { drawing = false },
+			background = { drawing = false },
+		}, override)
+	end
 end
 
 M.get_right_separator = function(color, override)
 	override = override or {}
-	return M.merge({
-		icon = {
-			string = "",
-			color = M.color[color],
-			font = M.font.separator,
-			padding_left = -16,
-			padding_right = 0,
-		},
-		label = { drawing = false },
-		background = { drawing = false },
-	}, override)
+	if style == "bubbles" then
+		return M.merge({
+			icon = {
+				string = "",
+				color = M.color[color],
+				font = M.font.separator,
+				padding_left = -16,
+			},
+			label = { drawing = false },
+			background = { drawing = false },
+		}, override)
+	else
+		return M.merge({
+			icon = {
+				string = "█",
+				color = M.color[color],
+				font = M.font.separator2,
+			},
+			label = { drawing = false },
+			background = { drawing = false },
+		}, override)
+	end
 end
 
 M.get_spacer = function(size, override)
