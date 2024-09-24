d364bd8e3466f7c9bd8e8baa4597f219eb8d19b8
xavierchanth <xchanthavong@gmail.com>
2024-09-24 07:44:56 -0400

    fix lazygit from dashboard

--- dotfiles/dot-config/nvim/lua/util/lazygit.lua
+++ dotfiles/dot-config/nvim/lua/util/lazygit.lua
@@ -7,20 +7,10 @@ function M.lazygit(opts)
   local wt = require("util.git_worktree")
   local iswt = wt.is_inside_worktree(opts.cwd)
   if not iswt then
-    wt.telescope(vim.tbl_deep_extend("force", {
-      attach_mappings = function(_, _)
-        local actions = require("telescope.actions")
-        actions.select_default:enhance({
-          pre = function()
-            wt.telescope(opts, function(path, _)
-              opts.cwd = path
-              LazyVim.lazygit(opts)
-            end)
-          end,
-        })
-        return true
-      end,
-    }, opts))
+    wt.telescope(opts, function(path, _)
+      opts.cwd = path
+      LazyVim.lazygit(opts)
+    end)
   else
     LazyVim.lazygit(opts)
   end
