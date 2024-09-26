4081a303d007dc7963850f93206940bbbaffe67f
xavierchanth <xchanthavong@gmail.com>
2024-09-26 00:02:33 -0400

    just use omnisharp extension..

--- dotfiles/dot-config/nvim/lua/plugins/lang/c_sharp.lua
+++ dotfiles/dot-config/nvim/lua/plugins/lang/c_sharp.lua
@@ -1,30 +1,3 @@
 return {
   { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "csharpier",
-        "netcoredbg",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        omnisharp = {
-          handlers = {
-            ["textDocument/definition"] = require("omnisharp_extended").definition_handler,
-            ["textDocument/typeDefinition"] = require("omnisharp_extended").type_definition_handler,
-            ["textDocument/references"] = require("omnisharp_extended").references_handler,
-            ["textDocument/implementation"] = require("omnisharp_extended").implementation_handler,
-          },
-          enable_roslyn_analyzers = true,
-          organize_imports_on_format = true,
-          enable_import_completion = true,
-        },
-      },
-    },
-  },
 }
