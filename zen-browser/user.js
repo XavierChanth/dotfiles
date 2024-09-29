8387f4338938509fda54cac127e74775be8a88d4
xavierchanth <xchanthavong@gmail.com>
2024-09-29 17:09:35 -0400

    clean up Zen config further

--- zen-browser/user.js
+++ zen-browser/user.js
@@ -4,12 +4,20 @@ user_pref("browser.contentblocking.category", "standard");
 user_pref("browser.newtabpage.pinned", "[]");
 user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("browser.tabs.inTitlebar", 1);
+user_pref("browser.newtabpage.activity-stream.showWeather", false);
+user_pref("browser.newtabpage.pinned", "[]");
 user_pref("browser.theme.content-theme", 0);
 user_pref("browser.theme.toolbar-theme", 0);
-user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\",\"contact_maxhu_dev-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring4\",\"urlbar-container\",\"customizableui-special-spring2\",\"downloads-button\",\"wrapper-sidebar-button\",\"developer-button\",\"unified-extensions-button\"],\"TabsToolbar\":[\"firefox-view-button\",\"tabbrowser-tabs\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"],\"zen-sidebar-top-buttons\":[\"zen-expand-sidebar-button\",\"zen-workspaces-button\",\"new-tab-button\"],\"zen-sidebar-icons-wrapper\":[\"sidebar-button\",\"_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action\",\"zen-profile-button\",\"preferences-button\"]},\"seen\":[\"developer-button\",\"_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action\",\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\",\"contact_maxhu_dev-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"TabsToolbar\",\"unified-extensions-area\",\"zen-sidebar-icons-wrapper\",\"zen-sidebar-top-buttons\"],\"currentVersion\":20,\"newElementCount\":27}");
+user_pref(
+  "browser.uiCustomization.state",
+  '{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","contact_maxhu_dev-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring4","urlbar-container","customizableui-special-spring2","downloads-button","wrapper-sidebar-button","developer-button","unified-extensions-button"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","alltabs-button"],"PersonalToolbar":["personal-bookmarks"],"zen-sidebar-top-buttons":["zen-expand-sidebar-button","zen-workspaces-button","new-tab-button"],"zen-sidebar-icons-wrapper":["sidebar-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","zen-profile-button","preferences-button"]},"seen":["developer-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","contact_maxhu_dev-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","TabsToolbar","unified-extensions-area","zen-sidebar-icons-wrapper","zen-sidebar-top-buttons"],"currentVersion":20,"newElementCount":27}',
+);
 user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
 user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo");
-user_pref("extensions.pictureinpicture.enable_picture_in_picture_overrides", true);
+user_pref(
+  "extensions.pictureinpicture.enable_picture_in_picture_overrides",
+  true,
+);
 user_pref("extensions.ui.dictionary.hidden", true);
 user_pref("extensions.ui.extension.hidden", false);
 user_pref("extensions.ui.lastCategory", "addons://list/extension");
@@ -21,9 +29,19 @@ user_pref("font.size.monospace.x-western", 14);
 user_pref("privacy.userContext.enabled", true);
 user_pref("signon.rememberSignons", false);
 user_pref("ui.textScaleFactor", 120);
-user_pref("zen.keyboard.shortcuts", "{\"zenSplitViewGrid\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"G\"},\"zenSplitViewVertical\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"V\"},\"zenSplitViewHorizontal\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"H\"},\"zenSplitViewClose\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"U\"},\"zenChangeWorkspace\":{\"ctrl\":true,\"alt\":false,\"shift\":true,\"meta\":false,\"key\":\"E\"},\"zenToggleCompactMode\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"C\"},\"zenToggleCompactModeSidebar\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"S\"},\"zenToggleCompactModeToolbar\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"T\"},\"zenToggleWebPanels\":{\"ctrl\":false,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"P\"}}");
+// user_pref(
+//   "zen.keyboard.shortcuts",
+//   `{"zenSplitViewGrid":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"G"},
+//     "zenSplitViewVertical":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"V"},
+//     "zenSplitViewHorizontal":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"H"},
+//     "zenSplitViewClose":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"U"},
+//     "zenChangeWorkspace":{"ctrl":true,"alt":false,"shift":true,"meta":false,"key":"E"},
+//     "zenToggleCompactMode":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"C"},
+//     "zenToggleCompactModeSidebar":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"S"},
+//     "zenToggleCompactModeToolbar":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"T"},
+//     "zenToggleWebPanels":{"ctrl":false,"alt":true,"shift":false,"meta":false,"key":"P"}}`,
+// );
 user_pref("zen.keyboard.shortcuts.disable-firefox", true);
-user_pref("zen.sidebar.data", "{\"data\":{\"p1727618888881\":{\"url\":\"https://calendar.google.com\",\"ua\":false}},\"index\":[\"p1727618888881\"]}");
 user_pref("zen.themes.updated-value-observer", true);
 user_pref("zen.view.sidebar-expanded", true);
 user_pref("zen.welcomeScreen.seen", true);
