eae50fc588ecd3d713888b4de9a72f8834c32747
xavierchanth <xchanthavong@gmail.com>
2024-09-29 02:13:16 -0400

    cleanup qmk

--- keyboards/qmk.sh
+++ keyboards/qmk.sh
@@ -1,30 +1,55 @@
 #!/bin/bash
+
+# https://stackoverflow.com/questions/2683279/how-to-detect-if-a-script-is-being-sourced
+(
+  [[ -n $ZSH_VERSION && $ZSH_EVAL_CONTEXT =~ :file$ ]] || 
+  [[ -n $KSH_VERSION && "$(cd -- "$(dirname -- "$0")" && pwd -P)/$(basename -- "$0")" != "$(cd -- "$(dirname -- "${.sh.file}")" && pwd -P)/$(basename -- "${.sh.file}")" ]] || 
+  [[ -n $BASH_VERSION ]] && (return 0 2>/dev/null)
+) && sourced=1 || sourced=0
+
+if [ $sourced -eq 0 ]; then
+  echo "This script is meant to be sourced, do not run directly"
+  echo "You will have the following commands available:"
+  echo "qmk_update, qmk_air75, qmk_voyager"
+  exit 0
+fi
+
 script_dir="$(dirname -- "$(readlink -f -- "$0")")"
 
 # Must be in the dir before trying to set the overlay_dir
-cd "$script_dir/qmk" || exit 1
-qmk config user.overlay_dir="$(realpath .)"
-qmk config user.keymap=xavierchanth
+qmk_init() {
+  (
+    cd "$script_dir/qmk" || return 1
+    qmk config user.overlay_dir="$(realpath .)"
+  )
+  qmk config user.keymap=xavierchanth
+}
+
+qmk_init
 
 # Note ZSA uses a separate repo to manage their keyboards so typical build methods will not work.
 # Use these helper scripts instead of following the docs
 qmk_update() {
   (
-    cd "$script_dir" || exit 1
+    cd "$script_dir" || return 1
     git submodule update --init --recursive qmk
     git submodule update --init --recursive zsa_firmware
-  ) || exit 1
+  ) || return 1
   qmk setup
 }
 
+# Waiting eons for https://github.com/qmk/qmk_firmware/pull/22751
+# Good thing I don't intend to flash this board any time soon
 qmk_air75() {
   qmk config user.qmk_home="$script_dir/qmk_firmware"
   qmk config user.keyboard=nuphy/air75_v2/ansi
+  qmk_init
   qmk compile
 }
 
 qmk_voyager() {
   qmk config user.qmk_home="$script_dir/zsa_firmware"
-  qmk config user.keyboard=zsa/voyager
+  qmk config user.keyboard=voyager
+  qmk_init
   qmk compile
 }
