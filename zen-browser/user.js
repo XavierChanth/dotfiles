3d25a166f6ba63ce51e36cb4136120047ac82390
xavierchanth <xchanthavong@gmail.com>
2024-09-30 08:33:26 -0400

    add new zen keyboard-shortcuts

--- zen-browser/user.js
+++ zen-browser/user.js
@@ -10,7 +10,7 @@ user_pref("browser.theme.content-theme", 0);
 user_pref("browser.theme.toolbar-theme", 0);
 user_pref(
   "browser.uiCustomization.state",
-  '{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","contact_maxhu_dev-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring4","urlbar-container","customizableui-special-spring2","downloads-button","wrapper-sidebar-button","developer-button","unified-extensions-button"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","alltabs-button"],"PersonalToolbar":["personal-bookmarks"],"zen-sidebar-top-buttons":["zen-expand-sidebar-button","zen-workspaces-button","new-tab-button"],"zen-sidebar-icons-wrapper":["sidebar-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","zen-profile-button","preferences-button"]},"seen":["developer-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","contact_maxhu_dev-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","TabsToolbar","unified-extensions-area","zen-sidebar-icons-wrapper","zen-sidebar-top-buttons"],"currentVersion":20,"newElementCount":27}',
+  '{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","contact_maxhu_dev-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring4","urlbar-container","customizableui-special-spring2","downloads-button","wrapper-sidebar-button","developer-button","unified-extensions-button"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","alltabs-button"],"PersonalToolbar":["personal-bookmarks"],"zen-sidebar-top-buttons":["zen-expand-sidebar-button","zen-workspaces-button","new-tab-button"],"zen-sidebar-icons-wrapper":["sidebar-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","zen-profile-button","preferences-button"]},"seen":["developer-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","contact_maxhu_dev-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","TabsToolbar","unified-extensions-area","zen-sidebar-icons-wrapper","zen-sidebar-top-buttons"],"currentVersion":20,"newElementCount":30}',
 );
 user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
 user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo");
@@ -32,17 +32,4 @@ user_pref("permissions.isolateBy.userContext", true);
 user_pref("privacy.userContext.enabled", true);
 user_pref("signon.rememberSignons", false);
 user_pref("ui.textScaleFactor", 120);
-// user_pref(
-//   "zen.keyboard.shortcuts",
-//   `{"zenSplitViewGrid":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"G"},
-//     "zenSplitViewVertical":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"V"},
-//     "zenSplitViewHorizontal":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"H"},
-//     "zenSplitViewClose":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"U"},
-//     "zenChangeWorkspace":{"ctrl":true,"alt":false,"shift":true,"meta":false,"key":"E"},
-//     "zenToggleCompactMode":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"C"},
-//     "zenToggleCompactModeSidebar":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"S"},
-//     "zenToggleCompactModeToolbar":{"ctrl":true,"alt":true,"shift":false,"meta":false,"key":"T"},
-//     "zenToggleWebPanels":{"ctrl":false,"alt":true,"shift":false,"meta":false,"key":"P"}}`,
-// );
-user_pref("zen.keyboard.shortcuts.disable-firefox", true);
 user_pref("zen.welcomeScreen.seen", true);
