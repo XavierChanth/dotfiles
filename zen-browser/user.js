11e07f8987a303e6981f934a4cf596eaf73e4d52
xavierchanth <xchanthavong@gmail.com>
2024-09-30 20:34:31 -0400

    clean up user js

--- zen-browser/user.js
+++ zen-browser/user.js
@@ -12,19 +12,6 @@ user_pref(
   "browser.uiCustomization.state",
   '{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","contact_maxhu_dev-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring4","urlbar-container","customizableui-special-spring2","downloads-button","wrapper-sidebar-button","developer-button","unified-extensions-button"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","alltabs-button"],"PersonalToolbar":["personal-bookmarks"],"zen-sidebar-top-buttons":["zen-expand-sidebar-button","zen-workspaces-button","new-tab-button"],"zen-sidebar-icons-wrapper":["sidebar-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","zen-profile-button","preferences-button"]},"seen":["developer-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","contact_maxhu_dev-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","TabsToolbar","unified-extensions-area","zen-sidebar-icons-wrapper","zen-sidebar-top-buttons"],"currentVersion":20,"newElementCount":30}',
 );
-user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
-user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo");
-user_pref("dom.security.https_only_mode", true);
-user_pref(
-  "extensions.pictureinpicture.enable_picture_in_picture_overrides",
-  true,
-);
-user_pref("extensions.ui.dictionary.hidden", true);
-user_pref("extensions.ui.extension.hidden", true);
-user_pref("extensions.ui.lastCategory", "addons://list/extension");
-user_pref("extensions.ui.locale.hidden", true);
-user_pref("extensions.ui.plugin.hidden", true);
-user_pref("extensions.ui.sitepermission.hidden", true);
 user_pref("font.name.monospace.x-western", "JetBrainsMono Nerd Font Mono");
 user_pref("font.size.monospace.x-western", 14);
 user_pref("permissions.isolateBy.privateBrowsing", true);
