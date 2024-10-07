f6252c95d643de1d03745b71a10011e866a40b1b
xavierchanth <xchanthavong@gmail.com>
2024-10-06 23:46:23 -0400

    update todo-comments config

--- dotfiles/dot-config/nvim/lua/plugins/editor.lua
+++ dotfiles/dot-config/nvim/lua/plugins/editor.lua
@@ -2,6 +2,16 @@ return {
   {
     "folke/todo-comments.nvim",
     opts = {
+      -- Had to override all of them so I could add highlighting to plurals
+      keywords = {
+        FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+        TODOS = { icon = " ", color = "info", alt = { "TODO" } },
+        HACKS = { icon = " ", color = "warning" },
+        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTES = { icon = " ", color = "hint", alt = { "NOTE", "INFO" } },
+        TESTS = { icon = "⏲ ", color = "test", alt = { "TEST", "TESTING", "PASSED", "FAILED" } },
+      },
       highlight = {
         pattern = [[.*<(KEYWORDS)\s*]],
       },
