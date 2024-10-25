d0010c43ca10768fec251778d3f182e399ea37ac
xavierchanth <xchanthavong@gmail.com>
2024-10-24 23:42:29 -0400

    remove yt filter

--- dotfiles/dot-config/qutebrowser/config.py
+++ dotfiles/dot-config/qutebrowser/config.py
@@ -35,22 +35,6 @@ colors = {
 color = colors.get(lastcolor, "macchiato")
 catppuccin.setup(c, color, True)
 
-# YouTube ad-block
-
-
-def filter_yt(info: interceptor.Request):
-    """Block the given request if necessary."""
-    url = info.request_url
-    if (
-        url.host() == "www.youtube.com"
-        and url.path() == "/get_video_info"
-        and "&adformat=" in url.query()
-    ):
-        info.block()
-
-
-interceptor.register(filter_yt)
-
 # config
 c.aliases = {
     "q": "close",
