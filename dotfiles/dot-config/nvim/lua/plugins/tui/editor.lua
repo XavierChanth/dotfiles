7abf31d3fc505056710c07763892c13f629451de
xavierchanth <xchanthavong@gmail.com>
2024-11-05 10:31:14 -0500

    add flash

--- dotfiles/dot-config/nvim/lua/plugins/tui/editor.lua
+++ dotfiles/dot-config/nvim/lua/plugins/tui/editor.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    },
+  },
+  {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile", "BufReadPre" },
     opts = {
