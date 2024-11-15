34959f24bdb94606600f95c6a73b0c5da471624b
xavierchanth <xchanthavong@gmail.com>
2024-11-15 00:31:43 -0500

    update display on unlock

--- dotfiles/dot-config/sketchybar/bar.lua
+++ dotfiles/dot-config/sketchybar/bar.lua
@@ -3,29 +3,16 @@ local opts = require("opts")
 
 local M = {}
 
-sbar.exec(
-	"system_profiler SPDisplaysDataType | grep -B 3 'Main Display:' | awk '/Display Type/ {print $3}' | grep -q 'Built-in'",
-	function(_, exit_code)
-		local pos = nil
-		if exit_code == 0 then
-			pos = "top"
-		else
-			pos = "bottom"
-		end
-
-		sbar.bar({
-			height = 40,
-			color = opts.color.base,
-			margin = 0,
-			sticky = true,
-			padding_left = 0,
-			padding_right = 0,
-			notch_width = 188,
-			display = "main",
-			position = pos,
-		})
-	end
-)
+sbar.bar({
+	height = 40,
+	color = opts.color.base,
+	margin = 0,
+	sticky = true,
+	padding_left = 0,
+	padding_right = 0,
+	notch_width = 188,
+	display = "main",
+})
 
 sbar.default({
 	background = {
--- dotfiles/dot-config/sketchybar/events.lua
+++ dotfiles/dot-config/sketchybar/events.lua
@@ -1,25 +1,47 @@
 local sbar = require("sketchybar")
-local bar = require("bar")
 
 local display_watcher
 local battery_watcher
-local woke_watcher
 local battery = {
 	percentage = 100,
 	charging = false,
 }
 
-local function update_display(_)
+local M = {}
+
+function M.setup_events()
+	sbar.add("event", "aerospace_workspace_change")
+	sbar.add("event", "builtin_display_change")
+	sbar.add("event", "screen_unlock", "com.apple.screenIsUnlocked")
+	display_watcher = sbar.add("item", {
+		drawing = false,
+		updates = true,
+	})
+	sbar.add("event", "battery_change")
+	battery_watcher = sbar.add("item", {
+		drawing = false,
+		update_freq = 60, -- check battery once per minute
+	})
+end
+
+local function is_builtin_display(cb)
 	sbar.exec(
 		"system_profiler SPDisplaysDataType | grep -B 3 'Main Display:' | awk '/Display Type/ {print $3}' | grep -q 'Built-in'",
 		function(_, exit_code)
-			if exit_code == 0 then
-				sbar.trigger("builtin_display_change", { is_builtin = "true" })
-			else
-				sbar.trigger("builtin_display_change", { is_builtin = "false" })
-			end
+			cb(exit_code == 0)
 		end
 	)
+end
+
+local function update_display(_)
+	is_builtin_display(function(is_builtin)
+		sbar.trigger("builtin_display_change", { is_builtin = tostring(is_builtin) })
+		if is_builtin then
+			sbar.bar({ position = "top" })
+		else
+			sbar.bar({ position = "bottom" })
+		end
+	end)
 end
 
 local function update_battery(_)
@@ -31,40 +53,21 @@ local function update_battery(_)
 	end)
 end
 
-return {
-	setup_events = function()
-		sbar.add("event", "aerospace_workspace_change")
-		sbar.add("event", "builtin_display_change")
-		display_watcher = sbar.add("item", {
-			drawing = false,
-			updates = true,
-		})
-		sbar.add("event", "battery_change")
-		battery_watcher = sbar.add("item", {
-			drawing = false,
-			update_freq = 60, -- check battery once per minute
-		})
-		woke_watcher = sbar.add("item", {
-			drawing = false,
-			updates = true,
-		})
-	end,
-	trigger_events = function()
-		display_watcher:subscribe("display_change", update_display)
-		display_watcher:subscribe("forced", update_display)
+function M.trigger_events()
+	display_watcher:subscribe("system_woke", update_display)
+	display_watcher:subscribe("screen_unlock", update_display)
+	display_watcher:subscribe("display_change", update_display)
+	display_watcher:subscribe("forced", update_display)
 
-		battery_watcher:subscribe("routine", update_battery)
-		battery_watcher:subscribe("forced", update_battery)
-		battery_watcher:subscribe("power_source_change", function(env)
-			local charging = (env.INFO == "AC")
-			if battery.charging ~= charging then
-				battery.charging = charging
-				sbar.trigger("battery_change", { percentage = battery.percentage, charging = charging })
-			end
-		end)
+	battery_watcher:subscribe("routine", update_battery)
+	battery_watcher:subscribe("forced", update_battery)
+	battery_watcher:subscribe("power_source_change", function(env)
+		local charging = (env.INFO == "AC")
+		if battery.charging ~= charging then
+			battery.charging = charging
+			sbar.trigger("battery_change", { percentage = battery.percentage, charging = charging })
+		end
+	end)
+end
 
-		woke_watcher:subscribe("system_woke", function()
-			update_display()
-		end)
-	end,
-}
+return M
