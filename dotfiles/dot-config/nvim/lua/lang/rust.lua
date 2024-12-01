35c53266eaac926b84313102a15e4f04ce3b4819
XavierChanth <xchanthavong@gmail.com>
2024-12-01 12:29:39 -0500

    rust fixes

--- dotfiles/dot-config/nvim/lua/lang/rust.lua
+++ dotfiles/dot-config/nvim/lua/lang/rust.lua
@@ -8,14 +8,6 @@ return {
     ft = { "rust" },
     opts = {
       server = {
-        on_attach = function(_, bufnr)
-          vim.keymap.set("n", "<leader>cR", function()
-            vim.cmd.RustLsp("codeAction")
-          end, { desc = "Code Action", buffer = bufnr })
-          -- vim.keymap.set("n", "<leader>dr", function()
-          --   vim.cmd.RustLsp("debuggables")
-          -- end, { desc = "Rust Debuggables", buffer = bufnr })
-        end,
         default_settings = {
           -- rust-analyzer language server configuration
           ["rust-analyzer"] = {
@@ -51,6 +43,7 @@ return {
       end
     end,
   },
+
   -- Additional plugins
   {
     "Saecki/crates.nvim",
