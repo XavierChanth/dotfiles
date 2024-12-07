4e92b701b1a593d907c4bfda1e092c8994b51038
XavierChanth <xchanthavong@gmail.com>
2024-12-07 12:19:22 -0500

    Fix copilot

--- dotfiles/dot-config/nvim/lua/plugins/copilot.lua
+++ dotfiles/dot-config/nvim/lua/plugins/copilot.lua
@@ -1,50 +1,54 @@
 return {
   {
-    -- Let's see if this thing is actually good or if I still think it's just
-    -- a distraction
-    "zbirenbaum/copilot.lua",
+    "github/copilot.vim",
     cmd = "Copilot",
-    -- add a config function to copilot setup
-    opts = {
-      suggestions = { enabled = true },
-      panel = { enabled = false },
-    },
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     build = "make tiktoken", -- Only on MacOS or Linux
-    keys = {
-      {
-        "<leader>a",
-        "",
-        mode = { "n", "v" },
-        desc = "+ai",
-      },
-      {
-        "<leader>aa",
-        function()
-          require("CopilotChat").toggle()
-        end,
-        mode = { "n", "v" },
-        desc = "CopilotChat",
-      },
-      {
-        "<leader>ap",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.fzflua").pick(actions.prompt_actions())
-        end,
-        desc = "CopilotChat - Prompt actions",
-      },
-    },
     opts = {
-      model = "gpt-4o",
-      -- model = "claude-3.5-sonnet",
-      auto_insert_mode = true,
+      -- config
+      model = "claude-3.5-sonnet",
+      -- auto_insert_mode = true,
+      -- style
       question_header = "  You ",
       answer_header = "  Copilot ",
       window = {
-        width = 0.4,
+        layout = "float",
+        border = "rounded",
+        width = 0.85,
+        height = 0.85,
+      },
+      -- maps
+      mappings = {
+        submit_prompt = {
+          insert = "<C-CR>",
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>a",
+        function()
+          local mode = vim.api.nvim_get_mode().mode
+          local input = vim.fn.input("Ask Copilot: ")
+
+          if input == "" then
+            require("CopilotChat").open()
+            return
+          end
+          require("CopilotChat").ask(input, {
+            selection = function(source)
+              local select = require("CopilotChat.select")
+              if mode == "v" then
+                return select.visual(source)
+              end
+              return select.buffer(source)
+            end,
+          })
+        end,
+        mode = { "n", "v" },
+        desc = "Copilot - Quick Chat",
       },
     },
   },
