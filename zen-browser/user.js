9d81dd26442c5d9ce7293ef6d82e981f3de7f9ff
XavierChanth <xchanthavong@gmail.com>
2024-12-16 15:02:36 -0500

    switch back to zen

--- zen-browser/user.js
+++ zen-browser/user.js
@@ -12,11 +12,17 @@ user_pref("browser.newtabpage.pinned", "[]");
 // Customization
 user_pref(
   "browser.uiCustomization.state",
-  '{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring4","urlbar-container","customizableui-special-spring2","downloads-button","wrapper-sidebar-button","developer-button","unified-extensions-button"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","alltabs-button"],"PersonalToolbar":["personal-bookmarks"],"zen-sidebar-top-buttons":["zen-expand-sidebar-button","zen-workspaces-button","new-tab-button"],"zen-sidebar-icons-wrapper":["_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","zen-profile-button","preferences-button"]},"seen":["developer-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","TabsToolbar","unified-extensions-area","zen-sidebar-icons-wrapper","zen-sidebar-top-buttons"],"currentVersion":20,"newElementCount":24}',
+  '{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","contact_maxhu_dev-browser-action","toggledarkmode_example_com-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring40","urlbar-container","customizableui-special-spring2","downloads-button","wrapper-sidebar-button","developer-button","unified-extensions-button"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","customizableui-special-spring37","alltabs-button"],"vertical-tabs":[],"PersonalToolbar":["personal-bookmarks"],"zen-sidebar-top-buttons":["customizableui-special-spring4","zen-expand-sidebar-button","new-tab-button","customizableui-special-spring38"],"zen-sidebar-icons-wrapper":["sidebar-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","zen-profile-button","preferences-button"]},"seen":["developer-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","contact_maxhu_dev-browser-action","toggledarkmode_example_com-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","TabsToolbar","unified-extensions-area","zen-sidebar-icons-wrapper","zen-sidebar-top-buttons","vertical-tabs"],"currentVersion":20,"newElementCount":42}',
 );
 user_pref("font.name.monospace.x-western", "JetBrainsMono Nerd Font Mono");
 user_pref("font.size.monospace.x-western", 14);
 user_pref("ui.textScaleFactor", 120);
+
+user_pref("zen.view.use-single-toolbar", false);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.toolbars.bookmarks.visibility", "newtab");
+user_pref("uc.bookmarks.hide-favicons", false);
+user_pref("uc.bookmarks.hide-folder-icons", true);
 
 // Privacy & security
 user_pref("permissions.isolateBy.privateBrowsing", true);
@@ -25,4 +31,4 @@ user_pref("privacy.userContext.enabled", true);
 user_pref("signon.rememberSignons", false);
 user_pref("security.ssl.require_safe_negotiation", true);
 user_pref("security.ssl.treat_unsafe_negotiation_as_broken", false);
-user_pref("security.OCSP.enabled", 1); // Doesn't work yet... but I want it on... I know what I am doing
+user_pref("security.OCSP.enabled", 1);
