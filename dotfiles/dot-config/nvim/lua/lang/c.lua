72880fc355d326598bf584570bdccadcc0d1b442
XavierChanth <xchanthavong@gmail.com>
2025-03-09 09:55:50 -0400

    feat: add header wrapper keymap for c

--- dotfiles/dot-config/nvim/lua/lang/c.lua
+++ dotfiles/dot-config/nvim/lua/lang/c.lua
@@ -63,6 +63,25 @@ return {
             "<cmd>ClangdSwitchSourceHeader<cr>",
             { buffer = event.buf, desc = "Switch Source/Header (C/C++)" }
           )
+          vim.keymap.set("n", "<leader>cw", function()
+            local filename = vim.fn.expand("%")
+            filename = filename:match("^.*/(.*/.*)$")
+            filename = filename:gsub("[-./]", "_")
+            filename = filename:upper()
+            -- top of file
+            vim.cmd.norm("ggO#ifndef " .. filename)
+            vim.cmd.norm("o#define " .. filename)
+            vim.cmd.norm("o#ifdef __cplusplus")
+            vim.cmd.norm('oextern "C" {')
+            vim.cmd.norm("o#endif")
+            vim.cmd.norm("o")
+            -- bottom of file
+            vim.cmd.norm("Go")
+            vim.cmd.norm("o#ifdef __cplusplus")
+            vim.cmd.norm("o}")
+            vim.cmd.norm("o#endif")
+            vim.cmd.norm("o#endif")
+          end, { buffer = event.buf, desc = "Wrap C headers" })
         end,
       },
     },
