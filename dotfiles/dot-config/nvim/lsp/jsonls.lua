10fc3d5609717ee82d3e7affe3974157ac45f74e
XavierChanth <xchanthavong@gmail.com>
2025-06-01 12:22:45 -0400

    feat: cleanup waybar

--- dotfiles/dot-config/nvim/lsp/jsonls.lua
+++ dotfiles/dot-config/nvim/lsp/jsonls.lua
@@ -7,6 +7,8 @@ return {
   },
   single_file_support = true,
   before_init = function(_, new_config)
+    new_config.settings = new_config.settings or {}
+    new_config.settings.yaml = new_config.settings.yaml or {}
     new_config.settings.yaml.schemas =
       vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
   end,
