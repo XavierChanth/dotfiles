ba6b60f28f706620a61bb8b9946d49418003e098
XavierChanth <xchanthavong@gmail.com>
2025-10-14 21:15:49 -0400

    feat: basic glide config

--- dotfiles/dot-config/glide/glide.ts
+++ dotfiles/dot-config/glide/glide.ts
@@ -15,3 +15,15 @@
 //   https://github.com/glide-browser/glide/blob/main/src/glide/browser/base/content/plugins/keymaps.mts
 //
 // Try typing `glide.` and see what you can do!
+glide.prefs.set("sidebar.verticalTabs", true)
+glide.prefs.set("browser.toolbarbuttons.introduced.sidebar-button", true)
+
+
+glide.keymaps.set("normal", "<leader>e", () => {
+  glide.keys.send("<C-A-z>")
+})
+
+// TODO for this browser
+// - Get password manager working
+// - Get FF containers working
+// - Explore keymaps for tab folders as a poor man's container?
