ee4449766711b1cc5345d4d843d040e43941a9ad
xavierchanth <xchanthavong@gmail.com>
2026-02-02 10:30:34 -0500

    feat: add formatter commands

--- dotfiles/dot-config/nvim/after/plugin/format.lua
+++ dotfiles/dot-config/nvim/after/plugin/format.lua
@@ -12,3 +12,12 @@ vim.api.nvim_create_user_command(
 vim.api.nvim_create_user_command("Format", function()
   require("conform").format({})
 end, {})
+
+-- Specific format methods
+
+vim.api.nvim_create_user_command("Jq", '<cmd>%!jq', {})
+
+-- You have to double % for prettier because it can't infer the filetype
+-- if you pipe over stdout. The first % tells nvim to replace the file, the
+-- second tells prettier where to read the file from.
+vim.api.nvim_create_user_command("Prettier", '<cmd>%!prettier %', {})
