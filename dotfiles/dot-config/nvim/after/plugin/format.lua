8b0340d97b6cc0a9bce081a97f142d1093d26098
xavierchanth <xchanthavong@gmail.com>
2026-02-02 11:00:52 -0500

    fix: use conform for prettier

--- dotfiles/dot-config/nvim/after/plugin/format.lua
+++ dotfiles/dot-config/nvim/after/plugin/format.lua
@@ -15,9 +15,8 @@ end, {})
 
 -- Specific format methods
 
-vim.api.nvim_create_user_command("Jq", '<cmd>%!jq', {})
+vim.api.nvim_create_user_command("Jq", ":%!jq", {})
 
--- You have to double % for prettier because it can't infer the filetype
--- if you pipe over stdout. The first % tells nvim to replace the file, the
--- second tells prettier where to read the file from.
-vim.api.nvim_create_user_command("Prettier", '<cmd>%!prettier %', {})
+vim.api.nvim_create_user_command("Prettier", function()
+  require("conform").format({ formatters = { "prettier" } })
+end, {})
