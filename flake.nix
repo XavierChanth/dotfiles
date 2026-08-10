24d0ad967fb71b220b5e7f1d63765429deb0fb46
xavierchanth <xchanthavong@gmail.com>
2026-08-10 14:49:58 -0400

    build(nix): track unstable platform inputs

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   description = "Cross-platform-ready dotfiles flake with nix-darwin and Home Manager";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    nix-darwin = { url = "github:LnL7/nix-darwin/nix-darwin-26.05"; inputs.nixpkgs.follows = "nixpkgs"; };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = { url = "github:nix-darwin/nix-darwin/master"; inputs.nixpkgs.follows = "nixpkgs"; };
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
     homebrew-core = { url = "github:homebrew/homebrew-core"; flake = false; };
     homebrew-cask = { url = "github:homebrew/homebrew-cask"; flake = false; };
     homebrew-rwx = { url = "github:rwx-cloud/homebrew-tap"; flake = false; };
-    home-manager = { url = "github:nix-community/home-manager/release-26.05"; inputs.nixpkgs.follows = "nixpkgs"; };
+    home-manager = { url = "github:nix-community/home-manager/master"; inputs.nixpkgs.follows = "nixpkgs"; };
     deploy-rs = { url = "github:serokell/deploy-rs"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
   outputs = inputs: import ./nix { inherit inputs; };
